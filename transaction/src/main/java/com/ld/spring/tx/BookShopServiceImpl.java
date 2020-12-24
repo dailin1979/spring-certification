cdb9d3fdd1b649965ef936a1818efff03954dccb
unknown <robbie.dailin@googlemail.com>
2020-12-25 00:01:27 +0100

    [impl] add annotation driven transaction: 1. add transactionmanager and annoation drive beans in xml and use annotation transactional before method declaration

--- transaction/src/main/java/com/ld/spring/tx/BookShopServiceImpl.java
+++ transaction/src/main/java/com/ld/spring/tx/BookShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.ld.spring.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service("bookShopService")
 public class BookShopServiceImpl implements BookShopService {
@@ -17,6 +18,8 @@ public class BookShopServiceImpl implements BookShopService {
   public BookShopServiceImpl() {
   }
 
+//  添加事务的注解
+  @Transactional
   @Override
   public void perchase(String username, String isbn) {
 //    1. 取得书的单价
